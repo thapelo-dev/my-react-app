c5742ffc09b5ae5c5cceb8bf4e23ed7865f9a9ca
Thapelo Mokoena <tmokoena2@DVTL102584.local>
2022-06-16 20:56:14 +0200

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { MantineProvider } from "@mantine/core";
+import "./App.css";
+import { AuthenticationForm } from "./components/Login/AuthenticationForm";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MantineProvider
+      withGlobalStyles
+      withNormalizeCSS
+      theme={{
+        breakpoints: {
+          xs: 500,
+          sm: 800,
+          md: 1000,
+          lg: 1200,
+          xl: 1400,
+        },
+      }}>
+      <AuthenticationForm style={{ width: "33%", margin: "30px auto" }} />
+    </MantineProvider>
   );
 }
 
